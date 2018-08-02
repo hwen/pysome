d67055c1c8efa4c0cf03633d0159793b661432e6
hwen <hwenleung@gmail.com>
2018-08-02 15:19:39 +0800

    auto battle logic

--- battle.py
+++ battle.py
@@ -33,6 +33,59 @@ def runAway():
 def suspendBattle():
   print('suspended...')
 
+def isIronBird():
+  return True
+
+def isOkDragonClaw():
+  return True
+
+def isGoodDragonClaw():
+  return True
+
+def isGoodEarthquake():
+  return True
+
+def useDragonClaw():
+  print('dragon claw')
+
+def useEarthquake():
+  print('earthquake')
+
+# 判断是否打倒一个
+def isFailOne():
+  print('fail one')
+  return True
+
+# 判断是否是允许出招的环境，是否网络延迟或倒下对话还没完成
+def isOkToMove():
+  print('hi is ok to move')
+  return True
+
+def autoUseMove():
+  if isOkToMove():
+    # 遇到盔甲鸟或者地震没有效果绝佳且龙爪有效的情况，使用龙爪
+    if isIronBird() or (isGoodEarthquake() == False and (isOkDragonClaw() or isGoodDragonClaw())):
+      useDragonClaw()
+    else:
+      useEarthquake()
+
+# 假定已经进入正式的战斗环境
+def battleForMoney():
+  while True:
+    autoUseMove()
+    # 等待出招和损伤计算
+    sleep(5)
+    if isFailOne():
+      pyautogui.press('b')
+      pyautogui.press('b')
+      pyautogui.press('b')
+    if isBattleEnd():
+      # 读完战斗结束后的会话
+      for i in range(8):
+        randomWait()
+        pyautogui.press('b')
+      break
+
 def shinyMove():
   pyautogui.keyDown('right')
   sleep(round(random.uniform(1.5, 1.8), 2))
@@ -44,7 +97,7 @@ def shinyMove():
 def findShiny():
   shinyRunCount = 0
   shinyMove()
-  while True:
+  while shinyRunCount < 100000:
     if isBattleStart():
       shinyRunCount += 1
       if isShiny():
